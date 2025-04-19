f34842fc1c179ecc5ca6a70ebcc35bf311d6461c
xezernn1 <hazarnovruz79@gmail.com>
2025-04-19 16:02:53 +0400

    vercel config edit

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 // server.js
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router('db.json')
+const router = jsonServer.router('db/db.json')
 const middlewares = jsonServer.defaults()
- 
+
 server.use(middlewares)
 server.use('', router)
-server.listen(process.env.PORT || 10000 || 5000, () => {
-  console.log('JSON Server is running')
+server.listen(3333, () => {
+  console.log('JSON Server is running port: 3333')
 })
